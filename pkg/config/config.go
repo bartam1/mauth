5ccc7da27207dccfb6a89a2dff3993d35435da2e
Marton Barta <bbartam@gmail.com>
2021-04-23 19:46:08 +0200

    feat: add token globals and log level to int

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"time"
+
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
@@ -8,16 +10,18 @@ import (
 var Global AppConfig
 
 type AppConfig struct {
-	SRV_HOST          string `mapstructure:"SRV_HOST"`
-	SRV_PORT          string `mapstructure:"SRV_PORT"`
-	LOCAL_ENV         string `mapstructure:"LOCAL_ENV"`
-	LOG_LEVEL         string `mapstructure:"LOG_LEVEL"`
-	DATABASE_PSQL_URL string `mapstructure:"DATABASE_PSQL_URL"`
+	SRV_HOST              string        `mapstructure:"SRV_HOST"`
+	SRV_PORT              string        `mapstructure:"SRV_PORT"`
+	LOCAL_ENV             string        `mapstructure:"LOCAL_ENV"`
+	LOG_LEVEL             int           `mapstructure:"LOG_LEVEL"`
+	DATABASE_PSQL_URL     string        `mapstructure:"DATABASE_PSQL_URL"`
+	TOKEN_SYMMETRIC_KEY   string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
+	ACCESS_TOKEN_DURATION time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
 }
 
 func LoadConfig(path string) (config AppConfig, err error) {
 	viper.AddConfigPath(path)
-	viper.SetConfigName("app")
+	viper.SetConfigName("mauth")
 	viper.SetConfigType("env")
 
 	//Override config file with env variables
