a35ba50a9b0fc3318d17b27bbd68cbbbbd6bdacf
Marton Barta <bbartam@gmail.com>
2021-04-29 23:41:20 +0200

    feat: new types, new things to log

--- internal/application/domain/domain_user.go
+++ internal/application/domain/domain_user.go
@@ -6,16 +6,66 @@ import (
 
 type User struct {
 	UserId       uint
-	UserType     UserType
-	UserName     UserName
-	UserSecret   UserSecret
-	FullName     FullName
-	Scopes       []Scope
-	RefreshToken string
-	AccessTokens []string
+	UserType     *UserType
+	UserName     *UserName
+	UserSecret   *UserSecret
+	Scopes       *Scopes
+	RefreshToken *string
+	AccessTokens []*string
 	CreatedAt    time.Time
-	LastLogTime  time.Time
-	LastLogIp    string
+	LastLogTime  []time.Time
+	LastLogIp    []*string
 	FailLogCount int
-	FailLogIp    string
+	FailLogTime  []time.Time
+	FailLogIp    []*string
+}
+
+func NewUser(UserType UType, UserName string, UserSecret string) (*User, error) {
+	ut, err := NewUserType(UserType)
+	if err != nil {
+		return nil, err
+	}
+	un, err := NewUserName(UserName)
+	if err != nil {
+		return nil, err
+	}
+	us, err := NewUserSecret(UserSecret)
+	if err != nil {
+		return nil, err
+	}
+	ns, err := NewScope(UScopeRead)
+	if err != nil {
+		return nil, err
+	}
+	usc := NewScopes(ns)
+	if err != nil {
+		return nil, err
+	}
+	lt := make([]time.Time, 5)
+	li := make([]*string, 5)
+	ft := make([]time.Time, 5)
+	fi := make([]*string, 5)
+	at := make([]*string, 5)
+	return &User{
+		UserId:       0,
+		UserType:     ut,
+		UserName:     un,
+		UserSecret:   us,
+		Scopes:       usc,
+		RefreshToken: nil,
+		AccessTokens: at,
+		CreatedAt:    time.Now(),
+		LastLogTime:  lt,
+		LastLogIp:    li,
+		FailLogCount: 0,
+		FailLogTime:  ft,
+		FailLogIp:    fi,
+	}, nil
+
+}
+
+type UserExtra struct {
+	UserId   uint
+	FullName *FullName
+	//UserEmail	*UserEmail
 }
